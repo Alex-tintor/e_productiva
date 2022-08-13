af50557be7ce90eef9782dd973a5862c2049a128
Alex-tintor <94770854+Alex-tintor@users.noreply.github.com>
2022-08-12 19:38:04 -0500

    vistas
    vista principal y login realizadas parcialmente, falta diseñar una lista para mostrar los diferentes roles del login, ademas de empezar a diseñar las otras vistas que faltan

--- src/app/core/inputs/inputs.component.ts
+++ src/app/core/inputs/inputs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-inputs',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inputs.component.css']
 })
 export class InputsComponent implements OnInit {
+
+  @Input() InputType:string="";
+  @Input() Titlle:string="";
+  @Input() Placeholder:string="";
 
   constructor() { }
 
