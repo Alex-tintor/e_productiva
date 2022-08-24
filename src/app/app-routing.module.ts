7ab4a2623b19a82e5285233779d4185caaf3d107
Alex-tintor <94770854+Alex-tintor@users.noreply.github.com>
2022-08-24 10:58:24 -0500

    login y routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import {Routes, RouterModule} from '@angular/router';
 import {ContratacionComponent} from '../app/views/principal/contratacion/contratacion.component';
 import {CoordinacionComponent} from '../app/views/principal/coordinacion/coordinacion.component';
 import {InstructorComponent} from '../app/views/principal/instructor/instructor.component';
-import {AppComponent} from '../app/app.component';
+import { PrincipalComponent } from './views/principal/principal.component';
 
 
 const routes: Routes = [
+  {path:'', component: PrincipalComponent},
   {path:'contratacion', component: ContratacionComponent},
   {path:'coordinacion', component: CoordinacionComponent},
   {path:'instructor', component: InstructorComponent},
-  {path:'componente', component: AppComponent}
 ];
 
 @NgModule({
