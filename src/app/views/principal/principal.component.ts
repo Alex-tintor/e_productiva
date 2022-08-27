af4424426f8b44a56122a9d725d765346619a88f
ozosena <laracardozojuansebastian@gmail.com>
2022-08-26 21:03:56 -0500

    Desarrollo Login
    Desarrollo parte visual del login y funcionalidad de validacion

--- src/app/views/principal/principal.component.ts
+++ src/app/views/principal/principal.component.ts
@@ -9,6 +9,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class PrincipalComponent implements OnInit {
 
   public loginForm:FormGroup;
+  public isLoading:boolean = false;
+  public loginFailed = false;
 
   constructor() {
     this.loginForm = new FormGroup({
@@ -20,9 +22,50 @@ export class PrincipalComponent implements OnInit {
     return
   }
 
-  public onSubmmit(){
-    console.log('funcionando al 100 mi rey')
+  public isInvalid(control:string):boolean{
+    return this.loginForm.controls[control].invalid && ( this.loginForm.controls[control].dirty || this.loginForm.controls[control].touched)
   }
 
-    
+  public getErrors(control:string):string{
+    switch(control){
+      case 'email':
+        return this.getErrorsByControl(control,"Ingresa Correo del Sena o Empresarial","No es un Correo Valido");
+      case 'password':
+        return this.getErrorsByControl(control,"Contraseña suministrada por Administracion","Debe ser mayor a 8 digitos y menor a 16");
+      default:
+        return "Ha ocurrido algo insesperado"
+    }
   }
+
+  public isFormValid():boolean{
+    return this.loginForm.valid;
+  }
+
+  private getErrorsByControl(control:string,defecto:string,errores:string):any{
+    if(this.isInvalid(control))
+      return errores
+    return defecto;
+  }
+
+  public onSubmmit(){
+    if(this.isFormValid()){
+      this.isLoading = true;
+      this.loginForm.disable()
+      this.loginFailed = false;
+      // Invocamos el llamado a login de la api
+      console.log(this.loginForm.value)
+      //Esta Linea se cambia por la Promesa de logeo login.subscribe(()=>{ // exito de loging},()=>{//fallo de login})
+      new Promise(resolve => setTimeout(resolve,10*1000)).then(()=>{
+        //si fue exitoso redirecciona
+        
+        //si fallo
+        this.loginForm.enable()
+        this.isLoading=false;
+        this.loginFailed=true;
+      })
+      return;
+    }
+    this.loginFailed = true;
+  }
+
+}
