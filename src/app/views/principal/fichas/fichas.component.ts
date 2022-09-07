8e85760cf1b22c5168c29808d8929c12fa71da04
Alex-tintor <94770854+Alex-tintor@users.noreply.github.com>
2022-09-07 13:30:14 -0500

    hola juan :d

--- src/app/views/principal/fichas/fichas.component.ts
+++ src/app/views/principal/fichas/fichas.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+<<<<<<< Updated upstream
 import { FichasService } from '../../../services/fichas.service'
 
+=======
+>>>>>>> Stashed changes
 @Component({
   selector: 'app-fichas',
   templateUrl: './fichas.component.html',
@@ -14,6 +17,45 @@ export class FichasComponent implements OnInit {
   ngOnInit(): void {
   }
 
+<<<<<<< Updated upstream
   
   
+=======
+  addFichas(){
+    this.fichas.push(
+    )
+    console.log('ficha agregada')
+  } 
+
+  public fichas=[
+    {
+      nombre: "ADSI",
+      n_ficha:2233518,
+      centro_correspondiente:"Servicios financieros",
+      etapa:"productiva",
+      estado:"En formacion"
+    },
+    {
+      nombre: "Tecnico en programación",
+      n_ficha:2233497,
+      centro_correspondiente:"Ni idea",
+      etapa:"productiva",
+      estado:"En formacion"
+    },
+    {
+      nombre: "Tecnico en redes",
+      n_ficha:4458799,
+      centro_correspondiente:"Centro",
+      etapa:"Lectiva",
+      estado:"desertado"
+    },
+    {
+      nombre: "Tecnico en mantenimiento",
+      n_ficha:3164887,
+      centro_correspondiente:"El el sena :D",
+      etapa:"Lectiva",
+      estado:"desertado"
+    }
+  ] 
+>>>>>>> Stashed changes
 }
