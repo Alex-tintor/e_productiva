f37fe41a99c355e01658fea8b051c4f552a3f8af
Alex-tintor <jalex_moral@hotmail.com>
2022-09-14 15:48:27 -0500

    try fusion
    try to do start function

--- src/app/views/principal/fichas/fichas.component.ts
+++ src/app/views/principal/fichas/fichas.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { concat } from 'rxjs';
 import { Ficha } from 'src/app/modules/fichas/Ficha'
 import { FichaService} from 'src/app/services/fichas.service'
 
@@ -16,17 +17,36 @@ export class FichasComponent implements OnInit {
   }
 
   fichasData = new FormGroup({
+    idFicha:new FormControl(0,[Validators.required,Validators.maxLength(10)]),
     nombre:new FormControl('',[Validators.required,Validators.maxLength(255)]),
-    idFicha:new FormControl('',[Validators.required,Validators.maxLength(10)]),
+    modalidad:new FormControl('',[Validators.required,Validators.maxLength(10)]),
+    instructorN:new FormControl('jose jose',[Validators.required,Validators.maxLength(45)]),
+    instructorA:new FormControl('arollo Diaz ',[Validators.required,Validators.maxLength(45)]),
     centroFormacion:new FormControl('',[Validators.required,Validators.maxLength(10)]),
-    etapa:new FormControl('',[Validators.required,Validators.maxLength(10)])
+    fechaInicio:new FormControl('',[Validators.required,Validators.maxLength(10)]),
+    fechaFin:new FormControl('',[Validators.required,Validators.maxLength(10)]),
+    etapa:new FormControl('',[Validators.required,Validators.maxLength(10)]),
+    estado:new FormControl('',[Validators.required])
   })
 
   ngOnInit(): void {return}
 
-  // addFicha(){
-  //   let ficha = new Ficha(this.nombre,this.numero,this.centro,this.etapa,this.estado);
-  //   this.fichaService.addFicha(ficha)
-  // }
+  addFicha(){
+    let numero = this.fichasData.controls['idFicha'].value;
+    let nombre = this.fichasData.controls['nombre'].value;
+    let modalidad = this.fichasData.controls['modalidad'].value;
+    let instructorN = this.fichasData.controls['instructorN'].value;
+    let instructorA = this.fichasData.controls['instructorA'].value;
+    let centro = this.fichasData.controls['centroFormacion'].value;
+    let fechaInicio = this.fichasData.controls['fechaInicio'].value;
+    let fechaFin = this.fichasData.controls['fechaFin'].value;
+    let etapa = this.fichasData.controls['etapa'].value;
+    let estado = this.fichasData.controls['estado'].value
+    let ficha = new Ficha(numero,nombre,modalidad,instructorN+instructorA,centro,fechaInicio,fechaFin,etapa,estado);
+    this.fichaService.addFicha(ficha)
+  }
+  show(){
+    console.log(this.addFicha())
+  }
   
 }
